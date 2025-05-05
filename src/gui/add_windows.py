b660cc8958d56a3211138e7b9658230ae8471e67
OP-jerbe <j.erbe@oregon-physics.com>
2025-05-05 10:17:44 -0700

    Alphabetizes classes

--- src/gui/add_windows.py
+++ src/gui/add_windows.py
@@ -30,20 +30,20 @@ from .error_messages import (
 )
 
 
-class AddUserWindow(QDialog):
+class AddCustomerWindow(QDialog):
     def __init__(self, parent=None) -> None:
         super().__init__(parent)
         self.create_gui()
 
     def create_gui(self) -> None:
         self.setFixedSize(300, 100)
-        self.setWindowTitle('Add New User')
+        self.setWindowTitle('Add New Customer')
 
         # Create form elements
-        self.name_label = QLabel('User Name:')
+        self.name_label = QLabel('Customer Name:')
         self.name_input = QLineEdit()
-        self.add_button = QPushButton('Add User')
-        self.add_button.clicked.connect(self.add_new_user)
+        self.add_button = QPushButton('Add Customer')
+        self.add_button.clicked.connect(self.add_customer)
 
         h_layout = QHBoxLayout()
         h_layout.addWidget(self.name_label)
@@ -55,13 +55,112 @@ class AddUserWindow(QDialog):
 
         self.setLayout(main_layout)
 
-    def add_new_user(self) -> None:
-        user_name = self.name_input.text()
+    def add_customer(self) -> None:
+        customer_name = self.name_input.text()
 
-        if add_user(user_name):
+        if add_customer(customer_name):
             self.accept()
         else:
-            add_user_failed_message(self)
+            add_customer_failed_message(self)
+
+
+class AddPartNumberWindow(QDialog):
+    def __init__(self, parent=None) -> None:
+        super().__init__(parent)
+        self.create_gui()
+
+    def create_gui(self) -> None:
+        self.setFixedSize(300, 150)
+        self.setWindowTitle('Add New Part Number')
+
+        # Create form elements
+        self.desc_label = QLabel('Select Product:')
+        self.desc_combo = QComboBox()
+        self.desc_combo.setStyleSheet('color: lightgreen;')
+        self.load_products()
+        self.number_label = QLabel('Part Number:')
+        self.number_input = QLineEdit()
+        self.add_button = QPushButton('Add Part Number')
+        self.add_button.clicked.connect(self.add_part_number)
+
+        v_label_layout = QVBoxLayout()
+        v_label_layout.addWidget(self.desc_label)
+        v_label_layout.addWidget(self.number_label)
+
+        v_input_layout = QVBoxLayout()
+        v_input_layout.addWidget(self.desc_combo)
+        v_input_layout.addWidget(self.number_input)
+
+        h_layout = QHBoxLayout()
+        h_layout.addLayout(v_label_layout)
+        h_layout.addLayout(v_input_layout)
+
+        main_layout = QVBoxLayout()
+        main_layout.addLayout(h_layout)
+        main_layout.addWidget(self.add_button)
+
+        self.setLayout(main_layout)
+
+    def load_products(self) -> None:
+        with SessionLocal() as session:
+            products = session.query(Product).order_by(Product.name).all()
+            for prod in products:
+                self.desc_combo.addItem(prod.name, prod.id)
+
+    def add_part_number(self) -> None:
+        product_id = self.desc_combo.currentData()
+        number = self.number_input.text()
+
+        if add_part_number(product_id, number):
+            self.accept()
+        else:
+            add_part_number_failed_message(self)
+
+
+class AddProductWindow(QDialog):
+    def __init__(self, parent=None) -> None:
+        super().__init__(parent)
+        self.create_gui()
+
+    def create_gui(self) -> None:
+        self.setFixedSize(300, 150)
+        self.setWindowTitle('Add New Product')
+
+        self.desc_label = QLabel('Product:')
+        self.desc_input = QLineEdit()
+
+        self.number_label = QLabel('Part Number:')
+        self.number_input = QLineEdit()
+
+        self.add_button = QPushButton('Add Product')
+        self.add_button.clicked.connect(self.add_product)
+
+        v_label_layout = QVBoxLayout()
+        v_label_layout.addWidget(self.desc_label)
+        v_label_layout.addWidget(self.number_label)
+
+        v_input_layout = QVBoxLayout()
+        v_input_layout.addWidget(self.desc_input)
+        v_input_layout.addWidget(self.number_input)
+
+        h_layout = QHBoxLayout()
+        h_layout.addLayout(v_label_layout)
+        h_layout.addLayout(v_input_layout)
+
+        main_layout = QVBoxLayout()
+        main_layout.addLayout(h_layout)
+        main_layout.addWidget(self.add_button)
+
+        self.setLayout(main_layout)
+
+    def add_product(self) -> None:
+        product_desc = self.desc_input.text()
+        product_num = self.number_input.text()
+
+        if add_product(product_desc, product_num):
+            self.accept()
+        else:
+            add_product_failed_message(self)
 
 
 class AddRMAWindow(QDialog):
@@ -196,119 +295,20 @@ class AddRMAWindow(QDialog):
             add_rma_failed_message(self)
 
 
-class AddProductWindow(QDialog):
-    def __init__(self, parent=None) -> None:
-        super().__init__(parent)
-        self.create_gui()
-
-    def create_gui(self) -> None:
-        self.setFixedSize(300, 150)
-        self.setWindowTitle('Add New Product')
-
-        self.desc_label = QLabel('Product:')
-        self.desc_input = QLineEdit()
-
-        self.number_label = QLabel('Part Number:')
-        self.number_input = QLineEdit()
-
-        self.add_button = QPushButton('Add Product')
-        self.add_button.clicked.connect(self.add_product)
-
-        v_label_layout = QVBoxLayout()
-        v_label_layout.addWidget(self.desc_label)
-        v_label_layout.addWidget(self.number_label)
-
-        v_input_layout = QVBoxLayout()
-        v_input_layout.addWidget(self.desc_input)
-        v_input_layout.addWidget(self.number_input)
-
-        h_layout = QHBoxLayout()
-        h_layout.addLayout(v_label_layout)
-        h_layout.addLayout(v_input_layout)
-
-        main_layout = QVBoxLayout()
-        main_layout.addLayout(h_layout)
-        main_layout.addWidget(self.add_button)
-
-        self.setLayout(main_layout)
-
-    def add_product(self) -> None:
-        product_desc = self.desc_input.text()
-        product_num = self.number_input.text()
-
-        if add_product(product_desc, product_num):
-            self.accept()
-        else:
-            add_product_failed_message(self)
-
-
-class AddPartNumberWindow(QDialog):
-    def __init__(self, parent=None) -> None:
-        super().__init__(parent)
-        self.create_gui()
-
-    def create_gui(self) -> None:
-        self.setFixedSize(300, 150)
-        self.setWindowTitle('Add New Part Number')
-
-        # Create form elements
-        self.desc_label = QLabel('Select Product:')
-        self.desc_combo = QComboBox()
-        self.desc_combo.setStyleSheet('color: lightgreen;')
-        self.load_products()
-        self.number_label = QLabel('Part Number:')
-        self.number_input = QLineEdit()
-        self.add_button = QPushButton('Add Part Number')
-        self.add_button.clicked.connect(self.add_part_number)
-
-        v_label_layout = QVBoxLayout()
-        v_label_layout.addWidget(self.desc_label)
-        v_label_layout.addWidget(self.number_label)
-
-        v_input_layout = QVBoxLayout()
-        v_input_layout.addWidget(self.desc_combo)
-        v_input_layout.addWidget(self.number_input)
-
-        h_layout = QHBoxLayout()
-        h_layout.addLayout(v_label_layout)
-        h_layout.addLayout(v_input_layout)
-
-        main_layout = QVBoxLayout()
-        main_layout.addLayout(h_layout)
-        main_layout.addWidget(self.add_button)
-
-        self.setLayout(main_layout)
-
-    def load_products(self) -> None:
-        with SessionLocal() as session:
-            products = session.query(Product).order_by(Product.name).all()
-            for prod in products:
-                self.desc_combo.addItem(prod.name, prod.id)
-
-    def add_part_number(self) -> None:
-        product_id = self.desc_combo.currentData()
-        number = self.number_input.text()
-
-        if add_part_number(product_id, number):
-            self.accept()
-        else:
-            add_part_number_failed_message(self)
-
-
-class AddCustomerWindow(QDialog):
+class AddUserWindow(QDialog):
     def __init__(self, parent=None) -> None:
         super().__init__(parent)
         self.create_gui()
 
     def create_gui(self) -> None:
         self.setFixedSize(300, 100)
-        self.setWindowTitle('Add New Customer')
+        self.setWindowTitle('Add New User')
 
         # Create form elements
-        self.name_label = QLabel('Customer Name:')
+        self.name_label = QLabel('User Name:')
         self.name_input = QLineEdit()
-        self.add_button = QPushButton('Add Customer')
-        self.add_button.clicked.connect(self.add_customer)
+        self.add_button = QPushButton('Add User')
+        self.add_button.clicked.connect(self.add_new_user)
 
         h_layout = QHBoxLayout()
         h_layout.addWidget(self.name_label)
@@ -320,10 +320,10 @@ class AddCustomerWindow(QDialog):
 
         self.setLayout(main_layout)
 
-    def add_customer(self) -> None:
-        customer_name = self.name_input.text()
+    def add_new_user(self) -> None:
+        user_name = self.name_input.text()
 
-        if add_customer(customer_name):
+        if add_user(user_name):
             self.accept()
         else:
-            add_customer_failed_message(self)
+            add_user_failed_message(self)
