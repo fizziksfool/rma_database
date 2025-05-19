5a674b04507327f8a24129c0bdf434ac87479a0d
OP-jerbe <j.erbe@oregon-physics.com>
2025-05-19 15:01:33 -0700

    Adds the custom_dropdown_style.py file to style the status filter in the open RMAs window.

---
+++ src/gui/custom_dropdown_style.py
@@ -0,0 +1,31 @@
+combo_style = """
+/*  QToolButton#statusFilterButton  */
+
+QToolButton#statusFilterButton {
+  color: #90ee90;  /* text color */
+  border: 2px solid #8bc34a;
+  border-radius: 0px;
+  border-top-left-radius: 4px;
+  border-top-right-radius: 4px;
+  width: 195px;
+  height: 24px;
+  background-color: rgba(35, 38, 41, 0.75);
+  border: 2px solid rgba(255, 255, 255, 0.2);
+  border-width: 0 0 2px 0;
+  border-bottom: 2px solid rgba(255, 255, 255, 51); /* 0.2 opacity */
+}
+
+/* QListWidget#clickableList */
+
+QListWidget#clickableList {
+  background-color: #232629;
+  border: 2px solid #31363b;
+  border-radius: 4px;
+}
+
+QListWidget::item:hover {
+  background-color: #bef67a;
+  color: black;
+}
+
+"""
